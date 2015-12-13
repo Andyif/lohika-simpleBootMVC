e9c4a9681837b57f0ec4540e0ca0ef787bb4b4bd
Andyif <andjey1990@gmail.com>
2015-12-13 22:35:04 +0200

    added simple MVC App with BOOT

---
+++ src/main/java/DeveloperRepository.java
@@ -0,0 +1,4 @@
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeveloperRepository extends CrudRepository<Developer, Long>{
+}
---
+++ src/main/java/SkillRepository.java
@@ -0,0 +1,4 @@
+import org.springframework.data.repository.CrudRepository;
+
+public interface SkillRepository extends CrudRepository<Skill, Long>{
+}
